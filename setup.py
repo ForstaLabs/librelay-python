fc4c3448bb7375ad6ec4b7dd80dddf4d766d4a19
Justin Mayfield <tooker@gmail.com>
2019-10-28 16:17:38 -0600

    rev up

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def long_desc():
 
 setup(
     name='librelay',
-    version='0.7.0',
+    version='0.7.1',
     description='Forsta messaging protocol library',
     author='Justin Mayfield',
     author_email='tooker@gmail.com',
